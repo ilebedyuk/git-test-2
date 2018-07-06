79b8098e05a420606e59b6c4479e20eba92fd880
Igor Lebedyuk <ilebeduyk@gmail.com>
2018-07-06 15:20:00 +0300

    5

--- src/main/java/com/example/demo/eee.java
+++ src/main/java/com/example/demo/eee.java
@@ -7,5 +7,6 @@ public class eee {
     public static void main(String[] args) {
         System.out.println("second commit");
         System.out.println("third commit");
+        System.out.println("third commit");
     }
 }
